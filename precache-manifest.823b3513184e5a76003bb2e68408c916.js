a47ee09eebb350fd5b16386b37654ec8f6799368
Анастасия <eugengamilov@gmail.com>
2020-08-01 20:32:30 +0300

    Updates

---
+++ precache-manifest.823b3513184e5a76003bb2e68408c916.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e52a24ae242d8e95c06fd28247ab920b",
+    "url": "/goit-react-hw-07-phonebook/index.html"
+  },
+  {
+    "revision": "e12a4e5631c71bb956fc",
+    "url": "/goit-react-hw-07-phonebook/static/css/main.73920ef4.chunk.css"
+  },
+  {
+    "revision": "f4b04fb467112b6b5a7e",
+    "url": "/goit-react-hw-07-phonebook/static/js/2.7c4078a4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react-hw-07-phonebook/static/js/2.7c4078a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e12a4e5631c71bb956fc",
+    "url": "/goit-react-hw-07-phonebook/static/js/main.a2134f82.chunk.js"
+  },
+  {
+    "revision": "8aecb1adf6dd54ad1d2a",
+    "url": "/goit-react-hw-07-phonebook/static/js/runtime-main.03f8d145.js"
+  },
+  {
+    "revision": "3facf487cefb590000ca613c204e5037",
+    "url": "/goit-react-hw-07-phonebook/static/media/backAddCss.3facf487.png"
+  }
+]);
